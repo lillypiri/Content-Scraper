d3809be6944a4c24828d561b3f4d2f04fe4ddc42
Lilly Piri <lilly@lillypiri.com>
2017-10-10 12:18:15 +1000

    initial commit - scraping and crawling works

---
+++ README.md
@@ -0,0 +1,19 @@
+#### Project 6 👕 🐸 
+
+## Content Scraper
+
+###### This is the sixth of twelve projects for the 🏡 Treehouse Techdegree Full Stack JavaScript.
+
+🚧 Currently WIP 🚧
+
+Crawls and scrapes content from [shirts4mike](http://www.shirts4mike.com/).
+
+
+On command line: 
+
+`npm install`
+
+then
+
+`npm start`
+
---
+++ scraper.js
@@ -0,0 +1,64 @@
+var request = require("request");
+var cheerio = require("cheerio");
+var fs = require("fs");
+var URL = require("url-parse");
+
+var data = './data';
+var START_URL = "http://shirts4mike.com/shirts.php";
+
+var pagesVisited = {};
+var pagesToVisit = [];
+var url = new URL(START_URL);
+var baseUrl = url.protocol + "//" + url.hostname + "/";
+
+pagesToVisit.push(START_URL);
+scrape();
+
+// if the data folder doesn't exist, make it
+if (!fs.existsSync(data)) {
+    fs.mkdirSync(data);
+}
+
+// visit each shirt page
+function scrape() {
+    var nextPage = pagesToVisit.pop();
+    if (nextPage in pagesVisited) {
+        scrape();
+    } else  if (nextPage === undefined) {
+        return;
+    } else {
+        visitPage(nextPage, scrape);
+    }
+}
+
+function visitPage(url, callback) {
+    console.log("Visiting " + url);
+    request(url, function(error, response, body) {
+      console.log("Status: " + response.statusCode);
+      // if there's an error, let the user know
+      if (response.statusCode !== 200) {
+          console.log("There's been a " + response.statusCode + " error");
+          fs.appendFileSync("scraper-error.log", "[" + Date() + "] : Error: " + response.statusCode + '\n');
+        callback();
+        return;
+      }
+      //get the price, title, url and image url from the product page and create a text file with the scraped data
+      var $ = cheerio.load(body);
+      var title = $("title").text();
+      var price = $("span.price").text();
+      var imgUrl = $("div.shirt-picture img").attr("src");
+      console.log("URL: " + url + "\n" + "Title: " + title + "\n" + "Price: " + price + "\n" + "Image URL: " + baseUrl + imgUrl + "\n");
+      fs.appendFileSync("test.txt", '\n' + "URL: " + url + "\n" + "Title: " + title + "\n" + "Price: " + price + "\n" + "Image URL: " + baseUrl + imgUrl + '\n');
+        collectShirtLinks($);
+        callback();
+    });
+}
+
+// get the links for the shirts and put them in an array we can use
+function collectShirtLinks($) {
+  var shirtLinks = $("a[href^='shirt.php?id=']");
+
+  shirtLinks.each(function() {
+    pagesToVisit.push(baseUrl + $(this).attr("href"));
+  });
+}
