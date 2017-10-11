1260a4d6ea94f802693419ee8024624951307e9f
Lilly Piri <lilly@lillypiri.com>
2017-10-11 23:33:32 +1000

    Better error handling

--- scraper.js
+++ scraper.js
@@ -14,7 +14,9 @@ function scrapeUrl(url) {
   return new Promise((resolve, reject) => {
     request(url, function(error, response, body) {
       if (error || response.statusCode !== 200) {
-        return reject(new Error("There was an error when scraping the site."));
+        return reject(
+          new Error("There was an error. Cannot connect to " + url)
+        );
       }
 
       //get the price, title, url and image url from the product page
@@ -45,8 +47,12 @@ function scrapeAll(urls) {
 function getShirtUrls(startUrl) {
   return new Promise((resolve, reject) => {
     request(startUrl, function(error, response, body) {
-      if (error || response.statusCode !== 200) {
+      if (error) {
         return reject(new Error("There was an error when scraping the site."));
+      } else if (response.statusCode !== 200) {
+        return reject(
+          new Error("There was an error. Cannot connect to " + startUrl)
+        );
       }
       var $ = cheerio.load(body);
       var url = new URL(START_URL);
@@ -61,32 +67,34 @@ function getShirtUrls(startUrl) {
 }
 
 function writeCSV(json) {
-    if (!fs.existsSync(data)) {
-      fs.mkdirSync(data);
-    }
+  if (!fs.existsSync(data)) {
+    fs.mkdirSync(data);
+  }
 
-    var now = new Date();
-    var csv_filename = "./data/" + now
-        .toISOString()
-        .substring(0, 10) + ".csv";
-    var fields = ["Title", "Price", "Image URL", "URL", "Time"];
+  var now = new Date();
+  var csv_filename = "./data/" + now.toISOString().substring(0, 10) + ".csv";
+  var fields = ["Title", "Price", "Image URL", "URL", "Time"];
 
-    var shirtscsv = json2csv({ data: json, fields: fields });
-    fs.writeFileSync(csv_filename, shirtscsv);
+  var shirtscsv = json2csv({ data: json, fields: fields });
+  fs.writeFileSync(csv_filename, shirtscsv);
 }
 
 const spinner = ora("Scraping shirts").start();
 
 getShirtUrls(START_URL)
   .then(shirtsUrls => {
-    spinner.text = 'Getting shirt information';
-    scrapeAll(shirtsUrls).then(shirts => {
-      spinner.text = "Writing to CSV file";
-      writeCSV(shirts);
-      spinner.succeed("CSV file written to ./data");
-    });
+    spinner.text = "Getting shirt information";
+    return scrapeAll(shirtsUrls);
+  })
+  .then(shirts => {
+    spinner.text = "Writing to CSV file";
+    writeCSV(shirts);
+    spinner.succeed("CSV file written to ./data");
   })
   .catch(error => {
     spinner.fail(error.message);
-    fs.appendFileSync("scraper-error.log", "[" + new Date() + "] : Error: " + error.message + "\n");
+    fs.appendFileSync(
+      "scraper-error.log",
+      "[" + new Date() + "] : Error: " + error.message + "\n"
+    );
   });
